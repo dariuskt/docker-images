643cd11605f69054f7496513f609b8a320741339
Darius <dariuskt@gmail.com>
2019-06-06 19:48:19 +0300

    adding platformio image for esp development

---
+++ dariuskt/esp-platformio/build/setup.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -x
+set -e
+
+echo force-unsafe-io > /etc/dpkg/dpkg.cfg.d/02apt-speedup
+
+
+apt-get update
+
+
+# vscode
+
+wget -O /tmp/vscode.deb 'https://go.microsoft.com/fwlink/?LinkID=760868'
+dpkg -i /tmp/vscode.deb || true
+apt-get install -y --no-install-recommends --fix-broken
+
+apt-get install -y --no-install-recommends \
+	libx11-xcb1 \
+	libxtst6 \
+	libasound2 \
+	x11-apps \
+	xvfb \
+
+
+# platformio
+
+sudo -HEu project code --install-extension platformio.platformio-ide --force
+ln -s /home/project/.platformio/penv/bin/platformio /usr/local/bin/platformio
+ln -s /home/project/.platformio/penv/bin/pio /usr/local/bin/pio
+ln -s /home/project/.platformio/penv/bin/piodebuggdb /usr/local/bin/piodebuggdb
+
+#apt-get install -y --no-install-recommends python-pip
+#pip install -U platformio
+
+
+cp -frv /build/files/* / || true
+
+
+source /usr/local/build_scripts/cleanup_apt.sh
+
+
