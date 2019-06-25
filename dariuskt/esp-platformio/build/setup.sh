696d5bb3bace0eabd14ac8be1122d76d010a7bcc
Darius <dariuskt@gmail.com>
2019-06-25 08:43:29 +0300

    disable extension recommendations

--- dariuskt/esp-platformio/build/setup.sh
+++ dariuskt/esp-platformio/build/setup.sh
@@ -24,17 +24,19 @@ apt-get install -y --no-install-recommends \
 	xauth \
 
 
-# platformio
 
+
+# platformio
 sudo -HEu project code --install-extension platformio.platformio-ide --force
 ln -s /home/project/.platformio/penv/bin/platformio /usr/local/bin/platformio
 ln -s /home/project/.platformio/penv/bin/pio /usr/local/bin/pio
 ln -s /home/project/.platformio/penv/bin/piodebuggdb /usr/local/bin/piodebuggdb
 
 
+
 # TODO:
 sudo -HEu project xvfb-run code --verbose &
-sleep 45
+sleep 60
 killall code || true
 
 sudo -HEu project pio platform install espressif8266 espressif32
@@ -45,6 +47,7 @@ usermod -a -G dialout project
 
 cp -frv /build/files/* / || true
 
+chown -R 1000:1000 /home/project
 
 source /usr/local/build_scripts/cleanup_apt.sh
 
